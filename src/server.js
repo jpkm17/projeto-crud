6731ef1977b3393d7951a23f77df464084d41240
João P. Meneguesso <jpm83633@gmail.com>
2024-01-13 12:08:23 -0300

    adicionando estrutura inicial

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+//definindo o template engine
+app.set('view engine', 'ejs')
+
+app.set('views', path.join(__dirname, 'views'))
+
+//definindo os arquivos públicos
+app.use(express.static(path.join(__dirname, 'public')))
+
+//habilita server para receber dados via post (formulario)
+app.use(express.urlencoded({extended:true}))
+
+
+//rotas
+app.get('/', (req, res) =>{
+    res.render('index',{
+        title:'Titulo teste'
+    })
+})
+
+//404 error (not found)
+app.use((req, res)=> { //middleware
+    res.send('Página não encontrada')
+})
+
+//executando o servidos
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
+console.log(`SERVER: http://localhost:8080/`)

d47d12ab486774e4714e4c595268effe2f060885
João P. Meneguesso <jpm83633@gmail.com>
2024-01-20 16:59:59 -0300

    conectando com o mongoDB

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+function connect() {
+    //funcionou sem essas duas 'sets'
+    //mongoose.set('useNewUrlParser', true)
+    //mongoose.set('useUnifiedTopology', true)
+    mongoose.connect('mongodb://localhost:27017')
+
+    const db = mongoose.connection
+
+    db.once('open', () => {
+        console.log('Connected to database!')
+    })
+
+    db.on('error', console.error.bind(console, 'connection error:'))
+}
+
+module.exports = {
+    connect
+}
